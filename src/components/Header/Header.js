11c22e2970fdf2b99469d585d2114a62600719f5
Akash Ahmed <akashahmed5724@gmail.com>
2022-06-03 20:56:25 +0600

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,12 +19,12 @@ function Header() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
                     <Nav className="space-x-2">
-                        <Nav.Link as={Link} to="/">HOME</Nav.Link>
-                        <Nav.Link as={Link} to="/services">SERVICES</Nav.Link>
-                        <Nav.Link as={Link} to="aboutUS">ABOUT US</Nav.Link>
-                        <Nav.Link as={Link} to="contactUs">CONTACT US</Nav.Link>
-                        <Nav.Link as={Link} to="career">CAREER</Nav.Link>
-                        <Nav.Link as={Link} to="work">WORK</Nav.Link>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/services">Services</Nav.Link>
+                        <Nav.Link as={Link} to="aboutUS">About us</Nav.Link>
+                        <Nav.Link as={Link} to="contactUs">Contact us</Nav.Link>
+                        <Nav.Link as={Link} to="career">Career</Nav.Link>
+                        <Nav.Link as={Link} to="work">Work</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
